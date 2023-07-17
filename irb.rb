7a842645e55c5c06312e6356d4cca1f277ba7e30
Jiachun Xiang <max997123@gmail.com>
2023-07-17 16:52:01 -0400

    Create irb.rb

---
+++ irb.rb
@@ -0,0 +1,22 @@
+2+2
+10/3
+10/3.0
+"Happy" + "coding"
+"Happy" + "\n" +  "coding"
+print "Happy" + "\n" +  "coding" + "\n"
+
+a = 3
+a
+a = "Hello"
+a
+b = "World!"
+puts(a + b)
+
+def hello(name)
+   puts("Hello #{name}!")
+ end
+ hello("David")
+
+ def add(a,b)
+    puts(a + b)
+  end
